ba9ad84afa476a5c2f9159a7b066da09a9802725
Ryan Nah <ryan.nah@tribex.co>
2024-06-12 19:23:10 +0800

    grandfinals and finals

--- pull_images.sh
+++ pull_images.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+retries=5
+
 # Function to pull images from a team's repository
 pull_images() {
     team=$1
     # You can specify the tags you want to pull here, for example: tags=("latest" "v1.0" "v2.0")
     images=("main" "autonomy" "nlp" "asr" "vlm")
-    
+    tags=("grandfinals" "finals")
     for image in "${images[@]}"; do
-        docker pull "asia-southeast1-docker.pkg.dev/dsta-angelhack/repository-$team/$team-$image:finals"
-        if ! [ $? -eq 0 ] ; then
-            docker pull "asia-southeast1-docker.pkg.dev/dsta-angelhack/repository-$team/$team-$image:latest"
-        fi
+        for tag in "${tags[@]}"; do
+            attempt=1
+            command="docker pull asia-southeast1-docker.pkg.dev/dsta-angelhack/repository-$team/$team-$image:$tag"
+            while [ $attempt -le $retries ]; do
+                echo "Attempt $attempt for $team for $image:$tag"
+                $command && break
+                ((attempt ++))
+                echo "Retrying..."
+                sleep 5
+            done
+        done
     done
 }
 
